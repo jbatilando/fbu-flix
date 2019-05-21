77df5e1d25a58888c38dd86b851857fc5de248a6
Miguel Batilando <miguel.batilando@gmail.com>
2019-05-21 13:06:22 -0700

    Initial Commit

---
+++ fbu_flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fbu_flix
+//
+//  Created by Miguel Batilando on 5/21/19.
+//  Copyright © 2019 Miguel Batilando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fbu_flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fbu_flix
+//
+//  Created by Miguel Batilando on 5/21/19.
+//  Copyright © 2019 Miguel Batilando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
